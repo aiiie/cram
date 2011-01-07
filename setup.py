2193e39b27ea6a12da38def9df152f1dffa090b3
Brodie Rao <brodie@bitheap.org>
2011-01-08 00:02:05 +1100

    setup.py: don't ignore errors in reading README.txt

--- setup.py
+++ setup.py
@@ -30,16 +30,7 @@ class test(Command):
         cram.main(['-v', 'tests'])
 
 def long_description():
-    try:
-        return open(os.path.join(sys.path[0], 'README.txt')).read()
-    except Exception:
-        return """
-Cram is a functional testing framework for command line applications
-based on Mercurial_'s `unified test format`_.
-
-.. _Mercurial: http://mercurial.selenic.com/
-.. _unified test format: http://www.selenic.com/blog/?p=663
-"""
+    return open(os.path.join(sys.path[0], 'README.txt')).read()
 
 setup(
     author='Brodie Rao',

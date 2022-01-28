c6fdbe793bd8434c74e5d66317476c51f9a0ad41
Nicola Coretti <nico.coretti@gmail.com>
2022-01-28 10:38:11 +0100

    Rename module _main.py to application.py

--- prysk/__init__.py
+++ prysk/__init__.py
@@ -1,6 +1,6 @@
 """Functional testing framework for command line applications"""
 
-from prysk._main import main
+from prysk.application import main
 from prysk._test import test, testfile
 
 __all__ = ['main', 'test', 'testfile']

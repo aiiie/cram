c798c4b9895e3d9a18e62cf2f94961365bda320c
Nicola Coretti <nico.coretti@gmail.com>
2022-01-30 08:53:58 +0100

    Add PROJECT_ROOT env var to integration tests

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+PROJECT_ROOT="$TESTDIR"/..
 # Bash doesn't expand aliases by default in non-interactive mode, so
 # we enable it manually if the test is run with --shell=/bin/bash.
 [ -n "$BASH" ] && shopt -s expand_aliases
@@ -7,25 +8,25 @@
 # The $PYTHON environment variable should be set when running this test
 # from Python.
 [ -n "$PYTHON" ] || PYTHON="`which python3`"
-[ -n "$PYTHONPATH" ] || PYTHONPATH="$TESTDIR/.." && export PYTHONPATH
+[ -n "$PYTHONPATH" ] || PYTHONPATH=${PROJECT_ROOT} && export PYTHONPATH
 if [ -n "$COVERAGE" ]; then
   if [ -z "$COVERAGE_FILE" ]; then
-    COVERAGE_FILE="$TESTDIR/../.coverage"
+    COVERAGE_FILE=${PROJECT_ROOT}/.coverage
     export COVERAGE_FILE
   fi
 
-  alias prysk="`which "$COVERAGE"` run -a --rcfile=$TESTDIR/../.coveragerc \
-$TESTDIR/../prysk --shell=$TESTSHELL"
-  alias doctest="`which "$COVERAGE"` run -a --rcfile=$TESTDIR/../.coveragerc \
+  alias prysk="`which "$COVERAGE"` run -a --rcfile=${PROJECT_ROOT}/.coveragerc \
+${PROJECT_ROOT}/prysk --shell=$TESTSHELL"
+  alias doctest="`which "$COVERAGE"` run -a --rcfile=${PROJECT_ROOT}/.coveragerc \
 $TESTDIR/run-doctests.py"
-  alias md5="$PYTHON $TESTDIR/../scripts/md5.py"
+  alias md5="$PYTHON ${PROJECT_ROOT}/scripts/md5.py"
 else
   PYTHON="`command -v "$PYTHON" || echo "$PYTHON"`"
   alias prysk="$PYTHON -m prysk --shell=$TESTSHELL"
   alias doctest="$PYTHON $TESTDIR/run-doctests.py"
-  alias md5="$PYTHON $TESTDIR/../scripts/md5.py"
+  alias md5="$PYTHON ${PROJECT_ROOT}/scripts/md5.py"
 fi
 
 # Copy in example tests
-cp -R "$TESTDIR"/../examples .
+cp -R ${PROJECT_ROOT}/examples .
 find . -name '*.err' -exec rm '{}' \;

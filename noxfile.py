86e4d680136b30be22a9b1fb8f0b099be9a21894
Nicola Coretti <nico.coretti@gmail.com>
2022-02-05 21:06:06 +0100

    Replace tox with nox

---
+++ noxfile.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+import nox
+
+BASEPATH = Path(__file__).parent.resolve()
+
+nox.options.sessions = [
+    "isort",
+    "code_format",
+    "check_manifest",
+    "pylint",
+    "integration",
+    "coverage",
+]
+
+
+def _common_prepare(session):
+    session.env["PYTHONPATH"] = f"{BASEPATH}"
+    session.install(f'-r{BASEPATH / "requirements.txt"}')
+    session.install(f"{BASEPATH}")
+
+
+@nox.session
+def code_format(session):
+    _common_prepare(session)
+    session.run(
+        "python",
+        "-m",
+        "black",
+        "--check",
+        "--diff",
+        "--color",
+        f"{BASEPATH}",
+    )
+
+
+@nox.session
+def isort(session):
+    _common_prepare(session)
+    session.run("python", "-m", "isort", "-v", "--check", f"{BASEPATH}")
+
+
+@nox.session
+def check_manifest(session):
+    _common_prepare(session)
+    session.run("python", "-m", "check_manifest", f"{BASEPATH}")
+
+
+@nox.session
+def pylint(session):
+    _common_prepare(session)
+    session.run("python", "-m", "pylint", f'{BASEPATH / "prysk"}')
+    session.run("python", "-m", "pylint", f'{BASEPATH / "scripts"}')
+
+
+@nox.session
+@nox.parametrize("shell", ["dash", "bash", "zsh"])
+def integration(session, shell):
+    _common_prepare(session)
+    session.env["TESTOPTS"] = f"--shell={shell}"
+    session.run(
+        "prysk", f"--shell={shell}", f'{BASEPATH / "test" / "integration" / "prysk"}'
+    )
+
+
+@nox.session
+def coverage(session):
+    _common_prepare(session)
+    session.env["COVERAGE"] = "coverage"
+    session.env["COVERAGE_FILE"] = f'{BASEPATH / ".coverage"}'
+    command = [
+        "coverage",
+        "run",
+        "-a",
+        f'--rcfile={BASEPATH / ".coveragerc"}',
+        "-m",
+        "prysk.cli",
+    ]
+    session.run(
+        *(command + ["--shell=bash", f'{BASEPATH / "test" / "integration" / "prysk"}'])
+    )
+    session.run(
+        *(command + ["--shell=dash", f'{BASEPATH / "test" / "integration" / "prysk"}'])
+    )
+    session.run(
+        *(command + ["--shell=zsh", f'{BASEPATH / "test" / "integration" / "prysk"}'])
+    )
+    session.run("coverage", "report", "--fail-under=97")
+    session.run("coverage", "lcov")
